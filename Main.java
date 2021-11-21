0600fde48ae8ae829520b5073497f9e9c24b3c94
klimilk1 <78904811+klimilk1@users.noreply.github.com>
2021-11-21 17:57:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,84 +1,108 @@
 package com.company;
-import java.util.Scanner;
-public class Main {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Введите массив: ");
-        int[] arr = {1, 3, 4, 5};
-        int[] arr2 = swapArr(arr);
-        for (int value: arr2){
-            System.out.print(value);
+import java.util.Comparator;
 
+class HubList<E> implements HubListImpl<E> {
+    int size = 0;
+    E[] list = (E[]) new Object[size];
+
+    @Override
+    public void add(E element) {
+        this.size++;
+        E[] newList = (E[]) new Object[this.size];
+        for (int i = 0; i < this.list.length; i++) {
+            newList[i] = this.list[i];
+        }
+        newList[this.size-1] = element;
+        this.list = newList;
+    }
+
+    @Override
+    public E removeAt(int index) {
+        E[] newList = (E[]) new Object[this.size-1];
+        E item = null;
+        int count = 0;
+        if (index > this.size-1) {
+            return null;
+        }
+        for (int i = 0; i < this.size; i++) {
+            if (index == i) {
+                item = list[i];
+            }
+            if (index != i) {
+                newList[count] = list[i];
+                count++;
+            }
+            if (i == this.size-1) {
+                this.list = newList;
+                this.size--;
+                return item;
+            }
+        }
+        return item;
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        for (int i = 0; i < this.size - 1; i++) {
+            for (int j = this.size - 1; j > i; j--) {
+                if (c.compare(this.list[i], this.list[j]) > 0) {
+                    E temp = this.list[j - 1];
+                    this.list[j - 1] = this.list[j];
+                    this.list[j] = temp;
+                }
+            }
         }
     }
-    public static int[] swapArr(int[] arr){
-        int first = arr[0];
-        int last = arr[arr.length-1];
-        arr[0] = last;
-        arr[arr.length-1] = first;
-        return arr;
+
+    @Override
+    public int find(E element) {
+        for (int i = 0; i < this.size; i++) {
+            if(this.list[i].equals(element)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void showData() {
+        for (E e : this.list) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
     }
 }
-//номер 9
-//import java.util.Arrays;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        int[] array1 = {1,2};
-//        int[] array2 = {3,4};
-//
-//        System.out.println(Arrays.toString(concat(array1.length, array1, array2.length, array2)));
-//    }
-//
-//    public static int[] concat(int array1Length, int[] array1, int array2Length, int[] array2) {
-//        int[] newArray = new int[array1Length + array2Length];
-//
-//        for (int i = 0; i < array1Length; i++) {
-//            newArray[i] = array1[i];
-//        }
-//
-//        for (int i = 0; i < array2Length; i++) {
-//            newArray[array1Length + i] = array2[i];
-//        }
-//
-//        return newArray;
-//    }
-//}
-//номер 4
-//public static void main(String [] args) {
-// Scanner input = new Scanner(System.in);
-// System.out.println("Введите первое число: ");
-// int a =input.nextInt();
-// System.out.println("Введите второе число: ");
-// int b =input.nextInt();
-// System.out.println("Введите третье число: ");
-// int c =input.nextInt();
-// if(a>b)
-// if(a>c)
-// System.out.println(a);
-// else {
-// if(b>c)
-// System.out.println(b);
-// }else {
-// System.out.println(c);
-// }
-//}
-//}
-//номер 3
-//System.out.println("Введите первое число: ");
-// int a =input.nextInt();
-// System.out.println("Введите второе число: ");
-// int b =input.nextInt();
-// if (a == b)
-// System.out.println(true);
-// else {
-// System.out.println(false);
-// }
-// }
-//}
 
+class HubComparator implements Comparator<Object> {
+    @Override
+    public int compare(Object a, Object b) {
+        if (a instanceof Integer) {
+            return Integer.compare((int) a, (int) b);
+        } else {
+            return -1;
+        }
+    }
+}
 
+interface HubListImpl<E> {
+    void add(E element);
+    E removeAt(int index);
+    void sort(Comparator<? super E> c);
+    int find(E element);
+}
 
-
-
+public class Main {
+    public static void main(String[] args) {
+        HubList<Integer> youList = new HubList<Integer>();
+        youList.add(4);
+        youList.add(578);
+        youList.add(3);
+        youList.showData();
+        System.out.println(youList.find(4));
+        System.out.println(youList.find(1));
+        youList.sort(new HubComparator());
+        youList.showData();
+        System.out.println(youList.removeAt(2));
+        youList.showData();
+    }
+}
